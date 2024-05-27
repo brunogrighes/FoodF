31eefc0e0bac303ecfbe35dd4b000ae3e0309f8b
brunogrighes <brunogrighes@gmail.com>
2024-05-27 02:04:40 -0300

    HOme

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,10 +1,17 @@
-/* eslint-disable no-unused-vars */
-import React from 'react'
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import Members from "../miniComponents/Members";
+import Qualities from "../miniComponents/Qualities";
+import Hero from "../miniComponents/Hero";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <Hero />
+      <Members />
+      <Qualities />
+    </>
+  );
+};
 
-export default Home
+export default Home;
