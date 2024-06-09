2c894cf89dd6b54c9b9ad50315ab37ad99d9ae5b
brunogrighes <brunogrighes@gmail.com>
2024-06-09 06:52:41 -0300

    Contatos

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -1,10 +1,82 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Contact = () => {
-  return (
-    <div>Contatos</div>
-  )
-}
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [message, setMessage] = useState("");
 
-export default Contact
+  const handleSendMessage = async (e) => {
+    e.preventDefault();
+    await axios
+      .post(
+        "http://localhost:4000/api/v1/message/send",
+        { name, email, phone, message },
+        {
+          withCredentials: true,
+          headers: { "Content-Type": "application/json" },
+        }
+      )
+      .then((res) => {
+        setName("");
+        setEmail("");
+        setPhone("");
+        setMessage("");
+        toast.success(res.data.message);
+      })
+      .catch((error) => {
+        console.error(error);
+        toast.error(error.response.data.message);
+      });
+  };
+
+  return (
+    <section className="contact">
+      <div className="container">
+       <div className="banner">
+          <form onSubmit={handleSendMessage}>
+            <h2>CONTATOS</h2>
+            <div>
+              <input 
+              type="text"
+              value={name} 
+              placeholder="Seu Nome"
+              onChange={(e) => setName(e.target.value)}
+               />
+
+              <input 
+              type="email"
+               value={email} 
+               placeholder="Seu Email"
+               onChange={(e) => setEmail(e.target.value)}
+               />
+              <input 
+              type="number"
+               value={phone} 
+               placeholder="Seu Numero"
+               onChange={(e) => setPhone(e.target.value)}
+               />  
+            </div>
+            <textarea  
+              rows={10}
+              value={message}
+              placeholder="Message"
+              onChange={(e) => setMessage(e.target.value)}
+            />
+
+          <button className="btn" type="submit">
+            Enviar Mensagem
+          </button>
+
+          </form>
+      </div>
+      </div>
+
+    </section>
+    );
+  };
+  
+  export default Contact;
