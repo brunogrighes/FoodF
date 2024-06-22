e6d29da654986eefe7d87e79363b192185823a30
brunogrighes <brunogrighes@gmail.com>
2024-06-22 12:07:57 -0300

    backend config

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+const cors = express
+
+const app = express();
+dotenv.config({ path: "./config.env" });
+
+export default app;
