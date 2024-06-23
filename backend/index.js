e2346e295755341391093e07c59a25fb48cab867
brunogrighes <brunogrighes@gmail.com>
2024-06-22 21:31:40 -0300

    server backend

--- backend/index.js
+++ backend/index.js
@@ -7,4 +7,16 @@ const cors = express
 const app = express();
 dotenv.config({ path: "./config.env" });
 
+app.use(
+    cors({
+        origin: [process.env.FRONTEND_URL],
+        method: ["POST"],
+        credentials: true,
+    })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
 export default app;
